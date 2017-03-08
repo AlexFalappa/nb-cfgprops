2427e2c42d6ba9c1a08208ea770e847e68a3e490
Alessandro Falappa <alex.falappa@gmail.com>
2017-03-08 18:26:57 +0100

    Fix initialization of the Set trough comma separated values

--- src/main/java/af/props/CollectionsProperties.java
+++ src/main/java/af/props/CollectionsProperties.java
@@ -10,29 +10,33 @@ import java.util.Set;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Java collections.
  * <p>
- * As per documentation only field getters are needed.
+ * The {@code set} field has the setter to support initialization from a string of comma separated values.
  *
  * @author Alessandro Falappa
  */
 @ConfigurationProperties(prefix = "collections")
-@Getter
 public class CollectionsProperties {
 
     /**
      * A set of doubles.
      */
+    @Getter
+    @Setter
     private Set<Double> set = new HashSet<>();
     /**
      * A list of strings.
      */
+    @Getter
     private List<String> list = new ArrayList<>();
     /**
      * A map of strings indexed by integers.
      */
+    @Getter
     private Map<Integer, String> map = new HashMap<>();
 
 }
